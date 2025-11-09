e236ce24ff375c6a6abde09d5efedad8813614d3
Muhammad Arif Raihan <arif@MacBook-Air.local>
2025-11-09 15:32:25 +0800

    fix img

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -2,12 +2,12 @@ import project1 from "../assets/images/project1.png";
 import project2 from "../assets/images/project2.png";
 import project3 from "../assets/images/project3.png";
 
-const projects = [
+const projectsData = [
   {
     id: 1,
     title: "Profil Kelurahan",
     description: "Description for Project 2",
-    image: {project1},
+    image: project1,
     badge: ["PHP", "JavaScript", "JQuery", "MySQL", "Bootstrap","CSS"],
     label: ["Web"],
   },
@@ -15,7 +15,7 @@ const projects = [
     id: 2,
     title: "Aplikasi Keamanan RT",
     description: "Description for Project 1",
-    image: {project2},
+    image: project2,
     badge: ["Flutter", "Firebase"],
     label: ["Mobile"],
   },
@@ -23,10 +23,10 @@ const projects = [
     id: 3,
     title: "Pengajuan Badge PKT",
     description: "Description for Project 3",
-    image: {project3},
+    image: project3,
     badge: ["Laravel", "Tailwind"],
     label: ["Web"],
   },
 ];
 
-export default projects;
+export default projectsData;
